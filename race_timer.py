f73a2fea3778d80e353d2525f6c350561f515b0b
aayinavo <129785313+aayinavo@users.noreply.github.com>
2023-04-18 19:02:37 +0300

    Create race_timer.py

---
+++ race_timer.py
@@ -0,0 +1,19 @@
+import time
+
+print("Готовы к старту?")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+print("Старт!")
+start_time = time.time()
+
+input("Нажмите Enter, чтобы остановить таймер.")
+end_time = time.time()
+
+race_time = end_time - start_time
+
+print("Ваше время: ", round(race_time, 2), "секунд.")
